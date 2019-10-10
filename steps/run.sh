c5a9620c033b25752caf2927ee11981bab8aebd8
Bin Lu <bin.lu@arm.com>
2019-10-10 14:49:42 +0800

    add iperf-arm64 support
    Signed-off-by: Bin Lu <bin.lu@arm.com>

--- steps/run.sh
+++ steps/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-CLIENTS=$(kubectl get pods -l app=iperf3-client -o name | cut -d'/' -f2)
+CLIENTS=$(kubectl get pods -l app=iperf-client -o name | cut -d'/' -f2)
 
 for POD in ${CLIENTS}; do
     until $(kubectl get pod ${POD} -o jsonpath='{.status.containerStatuses[0].ready}'); do
@@ -9,6 +9,6 @@ for POD in ${CLIENTS}; do
         sleep 5
     done
     HOST=$(kubectl get pod ${POD} -o jsonpath='{.status.hostIP}')
-    kubectl exec -it ${POD} -- iperf3 -c iperf3-server -T "Client on ${HOST}" $@
+    kubectl exec -it ${POD} -- iperf -c iperf-server -T "Client on ${HOST}" $@
     echo
 done
--- steps/setup.sh
+++ steps/setup.sh
@@ -3,8 +3,8 @@ set -eu
 
 kubectl create -f iperf3.yaml
 
-until $(kubectl get pods -l app=iperf3-server -o jsonpath='{.items[0].status.containerStatuses[0].ready}'); do
-    echo "Waiting for iperf3 server to start..."
+until $(kubectl get pods -l app=iperf-server -o jsonpath='{.items[0].status.containerStatuses[0].ready}'); do
+    echo "Waiting for iperf server to start..."
     sleep 5
 done
 
